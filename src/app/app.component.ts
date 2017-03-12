14967012ba3540fa008dbfd928f5315ed9de4d4d
Etienne Miret <etienne.miret@ens-lyon.org>
2017-03-12 13:12:11 +0100

    Create Tech class.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+export class Tech {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
 export class AppComponent  {
   title = 'Tour of technologies';
-  tech  = 'Angular';
+  tech: Tech = {
+    id: 1,
+    name: 'Angular'
+  }
 }
