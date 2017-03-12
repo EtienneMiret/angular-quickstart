c637291b01fb8c2d37f27ebebb0042b1b51c209d
Etienne Miret <etienne.miret@ens-lyon.org>
2017-03-12 18:24:43 +0100

    Add grades to technologies.

--- src/app/tech-list.ts
+++ src/app/tech-list.ts
@@ -3,42 +3,52 @@ import { Tech } from './tech';
 export const techs: Tech[] = [
   {
     id: 1,
+    grade: 10,
     name: 'Angular'
   },
   {
     id: 2,
+    grade: 8,
     name: 'EcmaScript'
   },
   {
     id: 3,
+    grade: 17,
     name: 'TypeScript'
   },
   {
     id: 4,
+    grade: 18,
     name: 'Java'
   },
   {
     id: 5,
+    grade: 15,
     name: 'Spring'
   },
   {
     id: 6,
+    grade: 12,
     name: 'JQuery'
   },
   {
     id: 7,
+    grade: 17,
     name: 'Node.js'
   },
   {
     id: 8,
+    grade: 7,
     name: 'Hibernate'
   },
   {
     id: 9,
+    grade: 14,
     name: 'Sass'
   },
   {
     id: 10,
+    grade: 16,
     name: 'Swift'
   }
 ];
--- src/app/tech.ts
+++ src/app/tech.ts
@@ -1,4 +1,5 @@
 export class Tech {
   id: number;
   name: string;
+  grade: number;
 }
