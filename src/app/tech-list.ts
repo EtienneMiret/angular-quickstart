aa0c43fed16b610195a351b8fab973d7b9575325
Etienne Miret <etienne.miret@ens-lyon.org>
2017-03-12 13:30:11 +0100

    Create a list of technologies.

---
+++ src/app/tech-list.ts
@@ -0,0 +1,44 @@
+import { Tech } from './tech';
+
+export const techs: Tech[] = [
+  {
+    id: 1,
+    name: 'Angular'
+  },
+  {
+    id: 2,
+    name: 'EcmaScript'
+  },
+  {
+    id: 3,
+    name: 'TypeScript'
+  },
+  {
+    id: 4,
+    name: 'Java'
+  },
+  {
+    id: 5,
+    name: 'Spring'
+  },
+  {
+    id: 6,
+    name: 'JQuery'
+  },
+  {
+    id: 7,
+    name: 'Node.js'
+  },
+  {
+    id: 8,
+    name: 'Hibernate'
+  },
+  {
+    id: 9,
+    name: 'Sass'
+  },
+  {
+    id: 10,
+    name: 'Swift'
+  }
+];
